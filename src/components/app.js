b8dfa3c69fa5f5ffaa15d6282ed3743fa4a801ef
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-10-16 19:25:23 +0300

    добавил функцию сортировки и отресовку в html

---
+++ src/components/app.js
@@ -0,0 +1,1 @@
+import './salo/index.js'
--- src/components/salo/index.js
+++ src/components/salo/index.js
@@ -1,11 +1,71 @@
-const constructor = document.querySelector('.container')
-constructor.innerHTML = `
-<h1 class="sale-title">Акции</h1>
-<div>
-  <label for="sale-sort">Сортировка:</label>
-  <input list="products" id="sale-sort" name="sale-sort" />
-  <datalist id="sale-sorts">
-    <option value="По умолчанию">
-  </datalist>
-</div>
-<ul></ul>`
+import template from './../../templates/template-sale.hbs';
+
+const a = [
+    {
+    }
+];
+
+const constructor = document.querySelector('.page-main .container');
+const sort = document.querySelector('#sale-sorts');
+
+function createSale (obj){
+  constructor.innerHTML = "";
+  const data = template(a);
+  constructor.insertAdjacentHTML('beforeend', data)
+//  const options = sort.querySelectorAll('option');
+  //  console.log(options);
+//  const a = document.querySelectorAll('option');
+//  console.dir(a);
+};
+createSale(a)
+// function sortSale (e){
+// let list, i, switching, items, shouldSwitch, dir;
+// let = switchcount = 0;
+//   list = document.querySelector(".sale-sort-list");
+//   switching = true;
+//   // Устанавливаем направление сортировки по возрастанию:
+//   dir = "asc";
+//   // Создаем цикл, который будет продолжаться до тех пор, пока переключение не будет выполнено:
+//   while (switching) {
+//     // Начнем с того, что переключение не выполняется:
+//     switching = false;
+//     items = list.getElementsByTagName("LI");
+//     // Перебираем все элементы списка:
+//     for (i = 0; i < (items.length - 1); i++) {
+//       // Начнем с того, что переключения быть не должно:
+//       shouldSwitch = false;
+//       /* Проверяем, должен ли следующий элемент поменяться местами с текущим,
+//       в зависимости от направления сортировки (по возрастанию или по убыванию: */
+//       if (dir == "asc") {
+//         if (items[i].innerHTML.toLowerCase() > items[i + 1].innerHTML.toLowerCase()) {
+//           /*  Если следующий элемент по алфавиту ниже текущего,
+//           пометить как переключатель и остановить цикл: */
+//           shouldSwitch = true;
+//           break;
+//         }
+//       } else if (dir == "desc") {
+//         if (items[i].innerHTML.toLowerCase() < items[i + 1].innerHTML.toLowerCase()) {
+//           /* Если следующий элемент по алфавиту выше текущего,
+//           пометить как переключатель и разорвать цикл: */
+//           shouldSwitch= true;
+//           break;
+//         }
+//       }
+//     }
+//     if (shouldSwitch) {
+//       /* Если переключатель был отмечен, делаем переключатель
+//       и отметьте, что переключение было выполнено: */
+//       items[i].parentNode.insertBefore(items[i + 1], items[i]);
+//       switching = true;
+//       // Каждый раз, когда переключение выполняется, увеличивайте switchcount на 1:
+//       switchcount ++;
+//     } else {
+//       /*  Если переключение не было выполнено И направление - "asc",
+//       установите направление «desc» и снова запустите цикл while. */
+//       if (switchcount == 0 && dir == "asc") {
+//         dir = "desc";
+//         switching = true;
+//       }
+//     }
+//   }
+// }
