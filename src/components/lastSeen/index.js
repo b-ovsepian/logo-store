2c39c2a5c40dcc15853de0a932a55e10be8646b7
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2020-10-16 10:38:39 +0300

    html

---
+++ src/components/lastSeen/index.js
@@ -0,0 +1,3 @@
+import refs from '../../refs/index.js';
+import './styles.css';
+// import lastSeenTemplate from './';
---
+++ src/refs/index.js
@@ -0,0 +1,10 @@
+export default {
+  //
+}
+{/* <li class="lastSeen-item">
+<!-- <img src="#" alt="#" class="lastSeen-img">
+<svg class="icon"><use href=""></use></svg> -->
+<p class="lastSeen-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+<p class="lastSeen-price">2000</p>
+<p class="lastSeen-newprice">1500</p>
+</li> */}
