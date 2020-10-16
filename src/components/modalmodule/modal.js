77ff6a4ed9b33c3bb3ae3561b3e10826b1a2ce73
TanyaChichkan <TanyaChichkan@users.noreply.github.com>
2020-10-16 13:01:26 +0300

    updated modal

--- src/components/modalmodule/modal.js
+++ src/components/modalmodule/modal.js
@@ -1,16 +1,10 @@
 import '../modalmodule/modal.css';
 import {modalrefs } from '../../refs/index';
 
-
-// insert element's and class's name as function's arguments
-
 const modalModule = (markup, listeners) => {
-    
        function openBackdrop() {
         modalrefs.lightbox.classList.add('is-open');
     };
-
-
     modalrefs.lightbox.addEventListener('click', e => {
         console.log(e.target);
         if ((e.target.classList.contains('backdrop')) ||
@@ -19,44 +13,34 @@ const modalModule = (markup, listeners) => {
             (e.target.classList.contains('icon-wrapper'))) {
             closeBackdrop();
         }
-
     });
-
     function closeBackdrop() {
         modalrefs.lightbox.classList.remove('is-open');
     };
-
     window.addEventListener('keydown', (e) => {
         if (e.code === 'Escape') {
             closeBackdrop();
         }
     });
-
     openBackdrop();
-
     const modalContent = document.querySelector('.modal-wrapper');
     modalContent.innerHTML = markup();
     listeners(closeBackdrop);
-  
 }
 
-
 const modalBTN = document.querySelector('.modalbtn');
-    modalBTN.addEventListener('click', () => product())
+modalBTN.addEventListener('click', product);
 
-
-const product = () => {
+    function product(){
     function buyGoods() {
-        
         // example for markup
         return `<div>hey<button class="hello">GET</button></div>`
     }
     function createListeners(closebackdrop) {
         const myButton = document.querySelector('.hello');
-        myButton.addEventListener("click", () => closebackdrop());
+        myButton.addEventListener("click", closebackdrop);
     }  
     modalModule(buyGoods, createListeners);
-
 }
 
 
