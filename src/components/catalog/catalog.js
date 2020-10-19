2269f5a0f004e5c350fb90c74ec8fad80255c03a
Igor Kichuk <66531978+kichman77@users.noreply.github.com>
2020-10-19 19:27:13 +0300

    catalog-img is not added

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -1,13 +1,21 @@
 import css from "./styles.css"
+import template from "./template.hbs"
+import categories from './data.js'
+const catalog = document.querySelector(".catalog")
+const item = template(categories)
+console.log(titleKbt);
+catalog.insertAdjacentHTML("beforeend", item)
 
 const titleKbt = document.getElementById("kbtTitle")
-const listKbt = document.getElementById("kbtList")
 const titleIt = document.getElementById("itTitle")
-const listIt = document.getElementById("itList")
 const titleHome = document.getElementById("homeTitle")
-const listHome = document.getElementById("homeList")
 const titleKitchen = document.getElementById("inTitle")
+
+const listKbt = document.getElementById("kbtList")
+const listIt = document.getElementById("itList")
+const listHome = document.getElementById("homeList")
 const listKitchen = document.getElementById("inList")
+
 const span1 = document.querySelector("#titleIcon1")
 const span2 = document.querySelector("#titleIcon2")
 const span3 = document.querySelector("#titleIcon3")
