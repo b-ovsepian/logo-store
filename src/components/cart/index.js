2f05855bd29168c297f87f0dce4fc7c9d40e63d3
Ilya Nikonenko <ilyanikonenko1613@gmail.com>
2020-10-19 16:21:54 +0300

    commit

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,33 +1,60 @@
 import css from '../cart/styles.css';
+import apiService from './apiService';
+import template from './templates/shopping-cart-template.hbs';
+import apiServic from './apiService.js';
+console.log(template);
 
-
-
-//Функция которая считает сумму
-// значение свойтсва цены из  обьекта товара умножить на значение textContent из спана
-
-
+const cartList = document.querySelector('.cart-list');
 const btnMin = document.querySelector('.decriment-button');
 const btnPlus = document.querySelector('.increment-button');
-const span = document.querySelector('.counter-value')
-const counter = {
-  count: 1,
+const span = document.querySelector('.counter-value');
+const itemSum = document.querySelector('.counter-sum');
+const item = document.querySelectorAll('.wrapper');
+const sum = document.querySelector('.sum');
 
-  plus() {
-    this.count += 1;
-    span.textContent = this.count;
-  },
-  minus() {
-    if (this.count > 1) {
-      this.count -= 1;
-      span.textContent = this.count;
-    }
-  },
-};
+function renderApi () {
+apiServic.fetchImage().then(dickhead => renderList(dickhead))
+}
+renderApi()
+// let result = document.querySelector('.result');
+// const counter = {
+//   count: 1,
+//   sum: 0,
 
-btnMin.addEventListener('click', () => {
-  counter.minus();
-});
+//   plus() {
+//     this.count += 1;
+//     span.textContent = this.count;
+//     sum.textContent = this.count * Number.parseInt(itemSum.textContent);
+//     const result1 = sum * this.count;
+//     result.textContent = result1;
+//   },
+//   minus() {
+//     if (this.count > 1) {
+//       this.count -= 1;
+//       span.textContent = this.count;
+//     }
+//   },
+//   sumAll() {
+//     const itemSumArr = document.querySelectorAll('.sum');
+//     itemSumArr.forEach(item => {
+//       this.sum = Number.parseInt(item.textContent);
+//     });
+//     result.textContent = this.sum;
+//   },
+// };
 
-btnPlus.addEventListener('click', ()=> {
-  counter.plus()
-})
+// btnMin.addEventListener('click', () => {
+//   counter.minus();
+// });
+
+// btnPlus.addEventListener('click', () => {
+//   counter.plus();
+//   counter.sumAll();
+// });
+
+
+function renderList(data) {
+  const list = template(data);
+  // const cartList = document.querySelector('.cart-list');
+  cartList.insertAdjacentHTML('beforeend', list);
+}
