9db945a9c7bc1ef2cd32ec6aadee2aaa5bf0b380
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-10-19 11:39:22 +0300

    Update index.js

--- src/components/developers/index.js
+++ src/components/developers/index.js
@@ -1,1 +1,7 @@
 import './developers.css';
+import developersTmpl from '../../templates/developers.hbs';
+import devJson from './developers.json';
+import refs from '../../refs';
+
+const markup = developersTmpl(devJson);
+refs.developersList.insertAdjacentHTML('beforeend', markup);
