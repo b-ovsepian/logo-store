4edb425606356dfb3f8b5bac7e17acfc6c870b19
AleksaHrevtosva <hrevtosva@yahoo.com>
2020-10-19 14:49:53 +0300

    changes styles.css & app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
+
 import developers from './developers';
 // import information from './information';
 import footer from './footer';
 import services from './services';
 import store from './store';
-=======
 import catalog from "./catalog/catalog.js"
 
->>>>>>> catalog
+
