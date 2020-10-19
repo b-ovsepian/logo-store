70166beb35bf7f81d873a8c99c881987cbd0e06e
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-10-19 15:41:04 +0300

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,3 +4,12 @@ import footer from './footer';
 import services from './services';
 import store from './store';
 import './category/category.js';
+
+// Тянем категории
+services.getCategories();
+setTokenToStore();
+
+function setTokenToStore() {
+  const localToken = localStorage.getItem('user_token');
+  localToken ? (store.auth.accces_token = localToken) : '';
+}
