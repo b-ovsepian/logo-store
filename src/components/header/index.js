7d9e0ee08737396bfe92ed55ee303cae12fda51d
redreren <jelka0503@gmail.com>
2020-10-20 15:02:43 +0300

    changes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,8 @@
 import css from './header.css';
+import refs from './refs';
 // import categories from '../../components/store';
 // const data = categories.categories;
 // console.log(data);
-const refs = {
-  logo: document.querySelector('.js-logo'),
-  phoneTrigger: document.querySelector('.js-phone'),
-  searchHeader: document.querySelector('.js-search'),
-  searachModal: document.querySelector('#search-modal'),
-  cart: document.querySelector('.js-cart'),
-  burger: document.querySelector('.js-burger'),
-  catalog: document.querySelector('.catalog'),
-  catalogBtn: document.querySelector('.js-catalog'),
-  catBtnIcon: document.getElementById('catBtnIcon'),
-  sale: document.querySelector('.js-sale'),
-  information: document.querySelector('.js-info'),
-  account: document.querySelector('.js-profile'),
-  likes: document.querySelector('.js-likes'),
-};
 refs.catalogBtn.addEventListener('click', () => {
   // тут отрисовка списка категорий
     // refs.catalog.classList.toggle('isHidden');
@@ -28,17 +14,3 @@ refs.catalogBtn.addEventListener('click', () => {
      catBtnIcon.textContent = '▼';
    }
 });
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
---
+++ src/components/header/refs.js
@@ -0,0 +1,15 @@
+export default {
+  logo: document.querySelector('.js-logo'),
+  phoneTrigger: document.querySelector('.js-phone'),
+  searchHeader: document.querySelector('.js-search'),
+  searachModal: document.querySelector('#search-modal'),
+  cart: document.querySelector('.js-cart'),
+  burger: document.querySelector('.js-burger'),
+  catalog: document.querySelector('.catalog'),
+  catalogBtn: document.querySelector('.js-catalog'),
+  catBtnIcon: document.getElementById('catBtnIcon'),
+  sale: document.querySelector('.js-sale'),
+  information: document.querySelector('.js-info'),
+  account: document.querySelector('.js-profile'),
+  likes: document.querySelector('.js-likes'),
+};
