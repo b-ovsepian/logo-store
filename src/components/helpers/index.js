1da41087ca02efefaae378ebc86270f3c20ab98e
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-10-20 10:27:57 +0300

    save code

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -5,11 +5,17 @@ export default {
     isDesktop: false,
     function() {
       if(window.innerWidth < 768){
-        this.isMobile = true
+        this.isMobile = true;
+        this.isTablet = false;
+        this.isDesktop = false;
        } else if(window.innerWidth < 1200 && window.innerWidth >= 768){
-        this.isTablet = true
+        this.isTablet = true;
+        this.isMobile = false;
+        this.isDesktop = false;
        } else if(window.innerWidth >= 1200){
-        this.isDesktop = true
+        this.isDesktop = true;
+        this.isMobile = false;
+        this.isTablet = false;
        }
     }
   }
