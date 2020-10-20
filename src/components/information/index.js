7cfbfb62ad98250d7395de36752648712c93ca6b
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-10-20 12:25:59 +0300

    Update index.js

--- src/components/information/index.js
+++ src/components/information/index.js
@@ -1,1 +1,17 @@
 import './information.css';
+import refs from '../../refs';
+import infoData from './information.js';
+import infoTmpl from '../../templates/information.hbs';
+
+function renderInformation() {
+  const ulList = document.createElement('ul');
+  ulList.classList.add('information-list');
+  ulList.classList.add('list');
+
+  const markup = infoTmpl(infoData);
+  ulList.insertAdjacentHTML('beforeend', markup);
+  document
+    .querySelector('.information')
+    .insertAdjacentElement('beforeend', ulList);
+}
+export default renderInformation;
