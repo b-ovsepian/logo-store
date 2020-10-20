883f07a444d072152317ad8d6e19f3bd82316d10
redreren <jelka0503@gmail.com>
2020-10-20 09:58:26 +0300

    header tablet

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,4 @@
 import developers from './developers';
+import header from './header';
 // import information from './information';
 import footer from './footer';
---
+++ src/components/header/index.js
@@ -0,0 +1,29 @@
+import css from './header.css';
+// const refs = {
+//     logo: document.querySelector('.js-logo'),
+//     phoneTrigger: document.querySelector('.js-phone'),
+//     searchHeader: document.querySelector('.js-search'),
+//     searachModal: document.querySelector('#search-modal'),
+//     cart: document.querySelector('.js-cart'),
+//     burger: document.querySelector('.js-burger'),
+//     catalog: document.querySelector('.js-catalog'),
+//     sale: document.querySelector('.js-sale'),
+//     information: document.querySelector('.js-info'),
+//     account: document.querySelector('.js-profile'),
+//     likes: document.querySelector('.js-likes'),
+// }
+
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]");
+//   const mobileMenuRef = document.querySelector("[data-menu]");
+
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded =
+//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+//     menuBtnRef.classList.toggle("is-open");
+//     menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+//     mobileMenuRef.classList.toggle("is-open");
+//   });
+// })();
