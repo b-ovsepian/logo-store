eeb65e2e7d63aaab34fa15329cecc53fca9fdb81
IC3CLD <67324553+IC3CLD@users.noreply.github.com>
2020-10-20 12:48:37 +0300

    slider
    v2.3

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,9 +1,10 @@
 import './styles.css';
 import slider from '../slider';
 
+
 const main = document.querySelector(`main`);
 const container = main.querySelector(`.container`);
-const array = [1, 2, 3, 4];
+const array = [1,2,3,4];
 
 container.insertAdjacentHTML(`afterbegin`, `<section class="hero"></section>`);
 const heroSection = document.querySelector(`.hero`);
