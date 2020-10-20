21451b8050fc29f4efc3b55dfad02751fd8f931f
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-10-20 11:48:14 +0300

    hhh

--- src/refs/index.js
+++ src/refs/index.js
@@ -1,18 +1,10 @@
 export default {
-<<<<<<< HEAD
     header: document.querySelector('.page-header'),
     main: document.querySelector('.page-main'),
     footer: document.querySelector('.page-footer'),
+
     developers: document.querySelector('.developers'),
     developersList: document.querySelector('.developers-list'),
-=======
-  header: document.querySelector('.page-header'),
-  main: document.querySelector('.page-main'),
-  footer: document.querySelector('.page-footer'),
 
-  developers: document.querySelector('.developers'),
-  developersList: document.querySelector('.developers-list'),
-
-  category: document.querySelector('.category'),
->>>>>>> 449882f9b3f25fb2d6a942abff5434184942fac8
+    category: document.querySelector('.category'),
 };
