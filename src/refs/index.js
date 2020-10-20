1a6bf475314d5d190f56b840cec16820567536cf
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-10-20 10:18:02 +0300

    коммит перед мерджем

--- src/refs/index.js
+++ src/refs/index.js
@@ -4,5 +4,4 @@ export default {
     footer: document.querySelector('.page-footer'),
     developers: document.querySelector('.developers'),
     developersList: document.querySelector('.developers-list'),
-    // cardList: document.querySelector(".card-list")
 };
