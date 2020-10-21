f0203987a26e585b61ddb5cdae6a4bbfbad48a2b
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-10-21 11:21:47 +0300

    Update index.js

--- src/components/telephone-trigger/index.js
+++ src/components/telephone-trigger/index.js
@@ -14,13 +14,6 @@ function renderTelephoneTrigger() {
     class="telephone-trigger__link"
     aria-label="call by vodafone"
   >
-    <img
-      class="telephone-trigger__logo"
-      src="./components/telephone-trigger/images/vodafone.svg"
-      alt="vodafone"
-      width="14"
-      height="14"
-    />
     +38 (050) 333-37-96
   </a>
   <a
@@ -28,13 +21,6 @@ function renderTelephoneTrigger() {
     class="telephone-trigger__link"
     aria-label="call by lifecell"
   >
-    <img
-      class="telephone-trigger__logo"
-      src="./components/telephone-trigger/images/lifecell.svg"
-      alt="lifecell"
-      width="14"
-      height="14"
-    />
     +38 (073) 837-37-37
   </a>
   <a
@@ -42,27 +28,17 @@ function renderTelephoneTrigger() {
     class="telephone-trigger__link"
     aria-label="call by kyivstar"
   >
-    <img
-      class="telephone-trigger__logo"
-      src="./components/telephone-trigger/images/kyivstar.svg"
-      alt="kyivstar"
-      width="14"
-      height="14"
-    />
     +38 (068) 877-37-37
   </a>
-</div>;`;
+  <div class="telephoneTrigger-icon"></div>
+</div>
+`;
 
       div.insertAdjacentHTML('beforeend', markup);
-      div.innerHTML += `<div class="icon-wrapper">
-<svg class="close-icon">
-<use href="./components/modalmodule/symbol-defs.svg#iconVector-16"></use>
-</svg>
-</div>`;
       return div.innerHTML;
     }
     function createListeners(closebackdrop) {
-      const myButton = document.querySelector('.icon-wrapper');
+      const myButton = document.querySelector('.telephoneTrigger-icon');
       myButton.addEventListener('click', closebackdrop);
     }
     modalModule(telephoneMarkup, createListeners);
