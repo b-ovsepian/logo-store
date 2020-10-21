9a91b55cd7e708d00ef06ccd081b800b3be72c92
TanyaChichkan <TanyaChichkan@users.noreply.github.com>
2020-10-21 17:26:53 +0300

    updated styles

--- src/components/newproducts/index.js
+++ src/components/newproducts/index.js
@@ -1,23 +1,35 @@
 import nprefs from './nprefs';
 import {cardItem} from '../carditem/index';
 import searchObj from '../services';
-// import css from '../newproducts/styles.css';
+import css from './styles.css';
 import getSlider from '../slider/index';
 
 
 console.log(nprefs.nplist);
+
 searchObj.searchProducts("",'new','100').then(data=>{
   let newArr= data.data;
-// //   cardItem(data, nprefs.nplist);
-// //   const arr=document.querySelectorAll('.card-item');
-// //   const itemArr=[...arr];
-// //   console.log(itemArr);
-// //   getSlider(itemArr, nprefs.npdiv, 2, false);
-// // });
 
 getSlider(newArr, nprefs.npdiv, 2, false, true);
 
+const sliderTrack=nprefs.npdiv.querySelector('.slider-track');
+sliderTrack.classList.add('np-track');
+
+const trackWidth=135*newArr.length+(newArr.length-1)*10+100;
+// const trackWidth=204*newArr.length+(newArr.length-1)*15;
+
+console.log(trackWidth);
+
+sliderTrack.style.width=`${trackWidth}px`;
+
+const sliderItem=sliderTrack.querySelectorAll('.card-item');
+
+sliderItem.forEach(el=>{
+  console.log(el);
+  el.classList.add('new-item');
+  // el.style.marginRight="10px"
 });
+
 
 // if(window.innerWidth < 768){
 //   elem = 6
@@ -26,3 +38,5 @@ getSlider(newArr, nprefs.npdiv, 2, false, true);
 //  } else if(window.innerWidth >= 1200){
 //   elem = 10
 //  }
+
+});
