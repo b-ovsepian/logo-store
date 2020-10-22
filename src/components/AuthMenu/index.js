804a397ec06aa458e6a292e78aba8be85a632f59
YehorSavin <yagaro4ek@gmail.com>
2020-10-22 10:44:14 +0300

    zxcvbn

--- src/components/AuthMenu/index.js
+++ src/components/AuthMenu/index.js
@@ -1,49 +1,50 @@
 import "./AuthMenu.css"
 import store from "../store"
 import {modalModule} from '../modalmodule/modal'
-// import "../authform/authform.js"
 import {openForm} from "../authform/authform.js"
-// const modalBTN = document.querySelector('.authbtn');
-// modalBTN.addEventListener('click', openForm);
+
 const iconHeaderAuth = document.querySelector('.icon-header-auth')
+const isAdmin = () =>  {
+    const info = JSON.parse(localStorage.getItem("info")) || {};
+    return info.role === "ADMIN"
+}
+const isUser = () => {
+    const info = JSON.parse(localStorage.getItem("info")) || {};
+    return info.role === "USER";
+}
+
 function userIgor(){
     function div() {
       return checkAuto()
     }
-    function createListeners(closebackdrop){
-        const myButton = document.querySelector(".button-close");
-        setTimeout(() => {
-            myButton.addEventListener("click", closebackdrop);
-        }, 1000);
-    }
+
     modalModule(div, createListeners)
 }
 
 const checkAuto =()=>{
-    if (localStorage.getItem("info").role === "USER") {
+    if (isUser()) {
         document.querySelector('modal-wrapper').innerHTML =  `<div class="private-account">
               <ul class="user-list">
-               <li><h2 class="user-heading">${store.user.name}</h2></li>
+               <li><h2 class="user-heading">${store.user.name?store.user.name:'Username'}</h2></li>
                <li class="user-element"><a class="user-link" href="">Личный кабинет</a></li>
                <li class="user-element"><a class="user-link" href="">Избранное</a></li>
                <button class="button-close">Выход</button>
              </ul>
-    </div>`
-    } else if (localStorage.getItem("info").role === "ADMIN") {
+        </div>`
+    } else if (isAdmin()) {
         document.querySelector('modal-wrapper').innerHTML =   `<div class="private-account">
-    <ul class="user-list">
-     <li><h2 class="user-heading">${store.user.name}</h2></li>
-     <li class="user-element"><a class="user-link" href="">Личный кабинет</a></li>
-     <li class="user-element"><a class="user-link" href="">Избранное</a></li>
-     <li class="user-element"><a class="user-link" href="">Создать обьявление</a></li>
-     <button class="button-close">Выход</button>
-   </ul>
-</div>`
+            <ul class="user-list">
+                <li><h2 class="user-heading">${store.user.name?store.user.name:'Username'}</h2></li>
+                <li class="user-element"><a class="user-link" href="">Личный кабинет</a></li>
+                <li class="user-element"><a class="user-link" href="">Избранное</a></li>
+                <li class="user-element"><a class="user-link" href="">Создать обьявление</a></li>
+                <button class="button-close">Выход</button>
+            </ul>
+        </div>`
     }
 
-    }
+}
 
-console.log(checkAuto);
 // const authMenu = () => {
 
 //     `<div class="private-account">
@@ -57,31 +58,38 @@ console.log(checkAuto);
 // </div>`
 // };
 
-// export default AuthMenu
-
-// const modalBTN = document.querySelector('.icon-header-auth');
-// modalBTN.addEventListener('click', product);
-
-const openHuman = document.querySelector('.icon-header-auth')
-// console.log(checkAuto());
 
 iconHeaderAuth.addEventListener("click",  (e) =>{
-    console.dir(e.target);
-    // checkAuto()
-    userIgor()
+    if (isAdmin() || isUser()) {
+        openAuthMenu()
+    } else {
+        openForm()
+    }
+
 })
+function openAuthMenu() {
+   modalModule(markup, createListenersMenu)
+}
 
-// function changeClose(){
+function markup() {
 
-//     function createListeners(closebackdrop) {
-//         const myButtonClose = document.querySelector('.button-close');
-//         myButtonClose.addEventListener("click", closebackdrop);
-//     }
-//     modalModule(AuthMenu , createListeners);
-// }
+        return  `<div class="private-account">
+            <ul class="user-list">
+            <li><h2 class="user-heading">${store.user.name?store.user.name:'Username'}</h2></li>
+            <li class="user-element"><a class="user-link" href="">Личный кабинет</a></li>
+            <li class="user-element"><a class="user-link" href="">Избранное</a></li>
+            ${isAdmin() ? '<li class="user-element"><a class="user-link" href="">Создать обьявление</a></li>' : ''}
 
-// function setTokenToStore() {
-//     const localToken = localStorage.getItem('user_token');
-//     localToken ? (store.auth.accces_token = localToken) : '';
-//   }
+            <button class="button-close">Выход</button>
+        </ul>
+    </div>`
+    }
+
+    function createListenersMenu(closebackdrop) {
+        const igorButtonClose = document.querySelector(".button-close");
+        igorButtonClose.addEventListener("click", closebackdrop);
+    }
+
+
+
 
