54bf0f3bf56e52b1d2e50e3325b35c58771cf40a
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2020-10-23 13:18:42 +0300

    finish logic

--- src/components/lastSeen/index.js
+++ src/components/lastSeen/index.js
@@ -2,6 +2,7 @@ import refs from '../../refs/index.js';
 import './styles.css';
 import store from '../store/index.js';
 import getSlider from '../slider/index.js';
+import { userSetter } from 'core-js/fn/symbol';
 
 const exclusiveArreyItems = [];
 function exclusiveArr(arr, elem) {
@@ -20,16 +21,17 @@ const longitude = arr.length //длина масива
     return arr //нет элемента и длина больше 10
   };
 };
-exclusiveArr(exclusiveArreyItems, {elem});//принимает масив и елемент на котором кликнули
+exclusiveArr(exclusiveArreyItems, ...user.lastSeen);//принимает масив и елемент на котором кликнули
 function getLocalStorage() {
   return JSON.parse(localStorage.getItem('lastSeen'))
 };
 function setLocalStorage(arrey) {
   return localStorage.setItem('lastSeen', JSON.stringify(arrey))
 };
-setLocalStorage(exclusiveArreyItems)
-
+setLocalStorage(exclusiveArreyItems);
+getLocalStorage('lastSeen');
 const lastSeenArr = getLocalStorage('lastSeen');
-console.log(lastSeenArr);
 // getSlider()
 getSlider(lastSeenArr, lastSeenSection, 2, false, true);
+
+
