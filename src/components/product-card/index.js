e28a21937f09fac40b194ee07e96cd40d93bb232
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-10-23 16:29:40 +0300

    fix big image

--- src/components/product-card/index.js
+++ src/components/product-card/index.js
@@ -4,7 +4,9 @@ import template from '../../templates/product-card-templates.hbs'
 import searchProducts from '../services/index.js'
 import obj from '../product-card/obj.json'
 import {commonRender} from '../cart/index.js'
-
+import services from '../services/index.js'
+import store from '../store/index.js'
+ 
 // renderImages(obj)
 
 let main = document.querySelector('.page-main')
@@ -12,7 +14,7 @@ let bigPhoto; //= document.querySelector('.product-card-slider-big-photo')
 let sliderList; //= document.querySelector('.product-card-slider-list')
 let productCart; //= document.querySelector('.product-card')
 let buyButton; //= document.querySelector('.product-card-button-buy')
-// console.log(productCart);
+let checkButton;// console.log(productCart);
 
 // createModalImg(images); 
 // console.log(images);
@@ -32,6 +34,8 @@ export default {
     bigPhoto = document.querySelector('.product-card-slider-big-photo')
     sliderList = document.querySelector('.product-card-slider-list')
     buyButton = document.querySelector('.product-card-button-buy')
+    checkButton = document.querySelector('.product-card-button-bookmarks')
+
     this.createModalImg(data)
   },
 
@@ -52,8 +56,34 @@ export default {
     sliderList.addEventListener('click', (e) => {
       bigImage.src = e.target.src
     })
+
+    setTimeout(() => {
+      store.user.favorites.forEach( _id => {
+        const heard = document.querySelector(`.product-card-button-bookmarks[data-id = "${_id}"]`)
+        if(heard){
+          heard.classList.add('product-card-button-bookmarks-full')
+        }
+      })
+    }, 1000);
+
+    checkButton.addEventListener('click', (e) => {
+      if(e.target.classList.contains('product-card-button-bookmarks')){
+        e.target.classList.toggle('product-card-button-bookmarks-full')
+        return services.addFavoriteProduct(e.target.dataset.id)
+      } 
+
+      if(e.target.classList.contains('product-card-button-bookmarks-full')){
+        e.target.classList.toggle('product-card-button-bookmarks')
+        return services.removeFavoriteProduct(e.target.dataset.id)
+      }
+    })
+
+
+
+
     let bigImage = document.createElement('img')
     bigImage.setAttribute('class', 'product-cart-main-img')
+    bigImage.style.display = "flex"
 
     bigImage.src = img[0].images[0]
     bigPhoto.append(bigImage)
