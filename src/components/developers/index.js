59c55ae18d1761d5c4aed9be50f6eb1893267faf
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-10-24 20:52:09 +0300

    Update index.js

--- src/components/developers/index.js
+++ src/components/developers/index.js
@@ -3,5 +3,15 @@ import developersTmpl from '../../templates/developers.hbs';
 import devJson from './developers.json';
 import refs from '../../refs';
 
-const markup = developersTmpl(devJson);
-refs.developersList.insertAdjacentHTML('beforeend', markup);
+function renderDevelopers() {
+  const markup = developersTmpl(devJson);
+  const developersList = document.createElement('ul');
+  const div = document.createElement('div');
+  div.classList.add('developers');
+  developersList.classList.add('developers-list');
+  developersList.classList.add('list');
+  developersList.insertAdjacentHTML('beforeend', markup);
+  div.insertAdjacentElement('beforeend', developersList);
+}
+
+export default renderDevelopers;
