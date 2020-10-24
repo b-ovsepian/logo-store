3b990c2f3061216fd80648eac080b579519e0590
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-10-24 20:13:57 +0300

    Update index.js

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,12 +1,11 @@
 import './styles.css';
 import slider from '../slider';
 
-
 const main = document.querySelector(`main`);
 const container = main.querySelector(`.container`);
-const array = ['','','',''];
+const array = ['', '', '', ''];
 
 container.insertAdjacentHTML(`afterbegin`, `<section class="hero"></section>`);
 const heroSection = document.querySelector(`.hero`);
 
-slider(array, heroSection, 1, false, false);
+slider(array, heroSection, 1, true, false);
